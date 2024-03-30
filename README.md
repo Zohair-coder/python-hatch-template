22fbf2027d72c406b4140cfc1e3a192f38c38e26
Zohair-coder <zohair.ul.hasan@gmail.com>
2024-03-30 16:49:59 -0400

    better docs and testing support

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 Template for a typical hatch project.
 
+Upgrade version of the project using:
+
+```
+hatch version [major|minor|micro]
+```
+
+Build the project using:
+
+```
+hatch build -c
+```
+
+Publish the project using:
+
+```
+hatch publish [-r <main|test>]
+```
+
+To run the tests, first install pytest:
+    
+```
+pip install pytest
+```
+
+Then install the project in editable mode:
+
+```
+pip install -e .
+```
+
+Then run the tests:
+
+```
+
+And finally, run the tests:
+
+```
+pytest tests
+```
--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,15 +1,14 @@
-import unittest
+import pytest
 
-# Be aware!
-# No need to know where in the filesystem the py-files are located.
-# This is because we can simply import the application package because
-# it is installed (in Developmend Mode via --editable).
-import helloworldcli
+# install the package in editable mode
+# pip install -e .
+# before running the tests
 
+import gflights
 
-class DummyTest(unittest.TestCase):
-    def test_dummy(self):
-        self.assertTrue(True)
+def test_dummy():
+    assert True
 
-    def test_import(self):
-        self.assertTrue(helloworldcli.just_return_true())
+def test_import():
+    assert gflights.just_return_true()
+
